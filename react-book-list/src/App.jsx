1610a4f54fcee7e0f8ac86aec7526a193310cf4b
Iman Hosseini Pour <Imanhpr1999@gmail.com>
2023-02-04 13:26:54 +0330

    add a dummy JSON-SERVER to save data

--- react-book-list/src/App.jsx
+++ react-book-list/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BookList from "./components/BookList";
 import BookCreate from "./components/CreateBook";
 import Book from "./models/Book";
@@ -6,21 +6,45 @@ import Book from "./models/Book";
 function App() {
   const [bookList, setBookList] = useState([]);
 
-  const createBook = (title) => {
-    const newBookList = [
-      ...bookList,
-      new Book({ id: Math.floor(Math.random() * 9999), name: title }),
-    ];
+  const fetchBooks = async () => {
+    const data = await (await fetch("http://localhost:8000/books")).json();
+    setBookList(data);
+  };
+
+  const deleteBook = async (book_id) => {
+    await fetch("http://localhost:8000/books/" + book_id, { method: "DELETE" });
+  };
+
+  const createBook = async (title) => {
+    const postBook = await fetch("http://localhost:8000/books", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name: title }),
+    });
+    if (postBook.status !== 201) {
+      console.log("ERROR");
+      return;
+    }
+    const jsonData = await postBook.json();
+    const newBookList = [...bookList, new Book(jsonData)];
     setBookList(newBookList);
     console.log(newBookList);
   };
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await deleteBook(id);
     const newBookList = bookList.filter((book) => {
       return book.id !== id;
     });
     setBookList(newBookList);
   };
-  const updateBookById = (id, title) => {
+  const updateBookById = async (id, title) => {
+    await fetch("http://localhost:8000/books/" + id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title: title }),
+    });
     const newBookList = bookList.map((book) => {
       if (book.id === id) {
         return new Book({ id, name: title });
@@ -29,6 +53,10 @@ function App() {
     });
     setBookList(newBookList);
   };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
   return (
     <div>
       <BookCreate onCreate={createBook} />
