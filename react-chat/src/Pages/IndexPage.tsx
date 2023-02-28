73d691d83c07bd3009c99196252bf178025317f7
Iman Hosseini Pour <Imanhpr1999@gmail.com>
2023-02-28 12:28:30 +0330

    Update index page of chat app

--- react-chat/src/Pages/IndexPage.tsx
+++ react-chat/src/Pages/IndexPage.tsx
@@ -1,4 +1,38 @@
+import { useState } from "react";
+import CenterSection from "../components/CenterSection";
+
 function IndexPage() {
-  return <h2>index</h2>;
+  const [roomName, setRoomName] = useState("");
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(roomName);
+  };
+  return (
+    <CenterSection>
+      <header>
+        <h2 className="font-bold text-blue-400 text-3xl capitalize">
+          join or create a room
+        </h2>
+      </header>
+      <form
+        onSubmit={submitHandler}
+        className="flex flex-col space-x-4 justify-center items-center sm:flex-row"
+      >
+        <label htmlFor="roomName">Room Name To Join :</label>
+        <input
+          onChange={(e) => setRoomName(e.target.value)}
+          type="text"
+          placeholder="Enter Room Id"
+          className="rounded font-bold bg-zinc-500 text- p-2 placeholder-gray-700 text-2xl text-amber-300"
+        />
+        <button className="rounded bg-blue-500 py-2 px-4 transition hover:bg-blue-800">
+          Join
+        </button>
+      </form>
+      <button className="rounded bg-yellow-500 text-black py-2 px-4 transition w-1/2 hover:bg-yellow-800 hover:text-light-400">
+        Create A Room
+      </button>
+    </CenterSection>
+  );
 }
 export default IndexPage;
