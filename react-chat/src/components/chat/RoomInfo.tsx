09d09c98aec334607977f4728e1aed904ad4ae0c
Iman Hosseini Pour <Imanhpr1999@gmail.com>
2023-03-01 14:21:51 +0330

    add copied to clipboard

--- react-chat/src/components/chat/RoomInfo.tsx
+++ react-chat/src/components/chat/RoomInfo.tsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from "react";
+
 function RoomInfo({ roomName }: { roomName: string }) {
+  const [copyFlag, setCopyFlag] = useState(false);
+  const copyHandler = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+    navigator.clipboard.writeText(e.currentTarget.textContent!);
+    setCopyFlag(true);
+    setTimeout(() => {
+      setCopyFlag(false);
+    }, 1000);
+  };
   return (
     <div className="border rounded-xl bg-dark-600 border-pink-500 p-4 col-start-4 row-end-2">
       <header>
         <h2 className="font-bold text-center text-2xl">Room Info</h2>
       </header>
-      <p className="text-lg">
-        Room Id: <span className="text-amber-400">{roomName}</span>
+      <p className="text-lg text-center">
+        Room Id:{" "}
+        <span
+          onClick={copyHandler}
+          className="text-amber-400 hover:cursor-pointer"
+        >
+          {roomName}
+        </span>
       </p>
+      {copyFlag && (
+        <p className="text-center text-sm text-rose-400">Copied To Clipboard</p>
+      )}
     </div>
   );
 }
