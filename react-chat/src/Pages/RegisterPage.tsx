35893e4131cb94af09890dd764cc0abd2d82bcf1
Iman Hosseini Pour <Imanhpr1999@gmail.com>
2023-03-04 09:52:02 +0330

    fix add register

--- react-chat/src/Pages/RegisterPage.tsx
+++ react-chat/src/Pages/RegisterPage.tsx
@@ -1,14 +1,35 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import Auth, { AuthFormState } from "../components/Auth";
+import { useNavigate } from "react-router-dom";
 
 export default function RegisterPage() {
+  const [errorMsg, setErrorMsg] = useState<null | string>(null);
+  const navigate = useNavigate();
   const registerRequest = useCallback(async (state: AuthFormState) => {
     const url = "http://localhost:8000/v1/auth/register";
+    const payload = JSON.stringify({
+      phoneNumber: state.number,
+      user_name: state.user_name,
+      password: state.password,
+      name: state.name,
+    });
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: payload,
+    });
+    if (response.ok) {
+      navigate("/auth/login", { replace: true });
+      setErrorMsg(null);
+      return;
+    }
+    setErrorMsg("Something went wrong.");
   }, []);
   return (
     <Auth
       title="Register a new user"
-      errorMsg={null}
+      errorMsg={errorMsg}
+      register
       onFormSubmit={registerRequest}
     ></Auth>
   );
